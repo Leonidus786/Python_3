47de431580d8de174efb08790ad901485b687dc2
Leonidus786 <abhi.upadh@gmail.com>
2024-10-10 21:04:26 +0530

    Primitive data types

---
+++ Day_2/Data_Types/README.md
@@ -0,0 +1,19 @@
+# Learn about the primitive data types in Python.
+
+1. Strings
+2. Integers
+3. Floats
+4. Booleans
+
+
+# len() -- to get the number of characters in a string or object.
+
+# id() -- to get the location of variable.
+
+# concatenation -- adding or combining two or more strings together.
+for eg -- "123" + "456"
+
+# type() -- to check the data type of variable or object.
+
+# typecasting -- to change the data type of a variable.
+
---
+++ Day_2/Data_Types/main.py
@@ -0,0 +1,57 @@
+# print(len("Hello, World!"))
+
+# a = "Abhishek"
+
+# print(len(a))
+
+
+# to get the location of variable -- id()
+# print(id(a))
+
+# print(a)
+
+# print("123" + "456")
+
+# print(123 + 456)
+
+
+# Declaring different type of variables
+
+A = 15
+
+B = 16.5
+
+C = 'Abhishek'
+
+D = True
+
+E = False
+
+
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+# print(E)
+
+# print(type(A))
+# print(type(B))
+# print(type(C))
+# print(type(D))
+# print(type(E))
+
+
+# a = float(A)
+#
+# print(A)
+# print(type(a))
+# print(a)
+
+
+# string_1 = str(B)
+
+# print(type(B))
+# print(B)
+
+# print(string_1)
+# print(type(string_1))
