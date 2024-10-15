7cf8807a96886fab683187612d5aa0337c8fafba
Leonidus786 <abhi.upadh@gmail.com>
2024-10-15 12:09:10 +0530

    Type error

---
+++ Day_2/Type Error and conversion/main.py
@@ -0,0 +1,36 @@
+# name = "Sunit"
+
+# print(type(name))
+
+# print(len(name))
+
+# print(len("Tanuj"))
+
+# print(type("Tanuj"))
+
+# print(len(12345))
+
+# print(len(str(12345678)))
+
+# a = 10
+
+# print(type(a))
+
+# a = str(a)
+
+# print(type(a))
+
+
+# print(type(12345))
+
+
+# b = 15
+# print(b)
+# b = float(b)
+# print(b)
+
+# c = str(b)
+
+# print(type(c))
+
+
